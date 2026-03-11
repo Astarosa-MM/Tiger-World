4a885504631f3e291ad06087cc31f9bb75717235
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-11 16:21:46 -0500

    curly -> straight quotes in 02_room_relations.sql

--- group5_db/ddl/tables/02_room_relations.sql
+++ group5_db/ddl/tables/02_room_relations.sql
@@ -7,7 +7,7 @@ primary key (room_A_ID, room_B_ID, connection_type),
 foreign key (room_A_ID) references room(room_ID),
 foreign key (room_B_ID) references room(room_ID),
 
-check (connection_type in (‘VERTICAL’, ’LATERAL’)),
+check (connection_type in ('VERTICAL', 'LATERAL')),
 check (room_A_ID <> room_B_ID)
 );
 

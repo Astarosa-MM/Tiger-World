8663678788a6a02dbbf6854fea434d96554fd9f4
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-11 15:54:45 -0500

    vertices -> 03_geometry.sql
    "create table" statement for vertices table added to 03_geometry.sql

--- group5_db/ddl/tables/03_geometry.sql
+++ group5_db/ddl/tables/03_geometry.sql
@@ -1,3 +1,15 @@
-(vertices)
+create table vertices (
+vertex_owner_type			varchar(8),
+vertex_owner_ID			bigint unsigned,
+
+vertex_ID				bigint unsigned auto_increment,
+vertex_type				varchar(9),
+x_coordinate				numeric (6,4),
+y_coordinate				numeric (7,4),
+
+primary key (vertex_ID),
+check (vertex_owner_type in (‘CAMPUS’, ‘BUILDING’, ‘FLOOR’, ‘ZONE’, ‘ROOM’)),
+check (vertex_type in (‘PERIMETER’, ’CENTROID’))
+);
 
 (edges)

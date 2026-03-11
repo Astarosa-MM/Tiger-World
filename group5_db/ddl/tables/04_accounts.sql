01370697e7249147794d5c8f4e535e79d1b7ae90
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-11 16:01:07 -0500

    user info -> 04_accounts.sql
    "create table" statement for user info table added to 04_accounts.sql

--- group5_db/ddl/tables/04_accounts.sql
+++ group5_db/ddl/tables/04_accounts.sql
@@ -1,4 +1,13 @@
-(user_info)
+create table user_info (
+
+user_ID			bigint unsigned auto_increment,
+email				varchar(255) unique,
+password_salt		varchar(16),
+password_hash		char(64),
+
+primary key (user_ID),
+check (email like '%.edu')
+);
 
 (user_settings)
 

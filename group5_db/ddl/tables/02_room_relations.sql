0ac54bfbc0a8f200d434ba6290d2d8e05b5e1d1d
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-11 15:51:19 -0500

    room_adjacency -> 02_room_relations.sql
    "create table" for room_adjacency added to 02_room_relations.sql

--- group5_db/ddl/tables/02_room_relations.sql
+++ group5_db/ddl/tables/02_room_relations.sql
@@ -1,3 +1,14 @@
-(room_adajcency)
+create table room_adjacency (
+room_A_ID			bigint unsigned,
+room_B_ID			bigint unsigned,
+connection_type		varchar(8),
+
+primary key (room_A_ID, room_B_ID, connection_type),
+foreign key (room_A_ID) references room(room_ID),
+foreign key (room_B_ID) references room(room_ID),
+
+check (connection_type in (‘VERTICAL’, ’LATERAL’)),
+check (room_A_ID <> room_B_ID)
+);
 
 (room_to_zone)

d0007cb8ff603ab6a3f81bc82a16765963402941
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-11 15:39:04 -0500

    indentation fix
    indentation fix for attributes in "campus" table

--- group5_db/ddl/tables/01_infrastructure.sql
+++ group5_db/ddl/tables/01_infrastructure.sql
@@ -1,7 +1,7 @@
 create table campus (
 campus_ID			bigint unsigned auto_increment,
-campus_name		varchar(255)
-campus_status		varchar(11)
+campus_name			varchar(255)
+campus_status			varchar(11)
 
 primary key (campus_ID),
 check (campus_status in (‘AVAILABLE’, ’UNAVAILABLE’))

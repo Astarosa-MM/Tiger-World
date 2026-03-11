cb164f555bff3216a6aac1807524b6013c1d1853
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-11 16:22:31 -0500

    curly -> straight quotes in 03_geometry.sql

--- group5_db/ddl/tables/03_geometry.sql
+++ group5_db/ddl/tables/03_geometry.sql
@@ -8,8 +8,8 @@ x_coordinate				numeric (6,4),
 y_coordinate				numeric (7,4),
 
 primary key (vertex_ID),
-check (vertex_owner_type in (‘CAMPUS’, ‘BUILDING’, ‘FLOOR’, ‘ZONE’, ‘ROOM’)),
-check (vertex_type in (‘PERIMETER’, ’CENTROID’))
+check (vertex_owner_type in ('CAMPUS', 'BUILDING', 'FLOOR', 'ZONE', 'ROOM')),
+check (vertex_type in ('PERIMETER', 'CENTROID'))
 );
 
 create table edges (

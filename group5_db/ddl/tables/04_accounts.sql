07b1ff4519226e622f124eccb881cc86310e7f27
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-11 16:24:14 -0500

    curly -> straight quotes in 04_accounts.sql

--- group5_db/ddl/tables/04_accounts.sql
+++ group5_db/ddl/tables/04_accounts.sql
@@ -12,18 +12,18 @@ create table user_settings (
 user_ID				bigint unsigned,
 default_campus			bigint unsigned,
 mode				varchar(5),
-time_zone			char(3),
+time_zone			varchar(4),
 push_notifs			varchar(3),
 
 primary key (user_ID),
 foreign key (user_ID) references user_info(user_ID),
 foreign key (default_campus) references campus(campus_ID),
 check(mode in (
-‘LIGHT’, ‘DARK’)),
+'LIGHT', 'DARK')),
 check(time_zone in (
-‘EST’, ‘CST’, ‘MST’, ‘PST’, ‘AKST’, ‘HST’)),
+'EST', 'CST', 'MST', 'PST', 'AKST', 'HST')),
 check(push_notifs in (
-‘ON’, ‘OFF’))
+'ON', 'OFF'))
 );
 
 create table user_bookmarks (
@@ -34,5 +34,5 @@ user_ID			bigint unsigned,
 primary key (user_ID, bookmarked_type, bookmarked_ID),
 foreign key (user_ID) references user_info(user_ID),
 check(bookmarked_type in (
-‘CAMPUS’, ‘BUILDING’, ‘FLOOR’, ‘ZONE’, ‘ROOM’))
+'CAMPUS', 'BUILDING', 'FLOOR', 'ZONE', 'ROOM'))
 );

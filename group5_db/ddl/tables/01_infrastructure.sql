2e5f7b2827d9e529397ee469ac62a592c76df18f
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-11 15:38:19 -0500

    building -> 01_infrastructure.sql
    "create table" statement for building table has been added to 01_infrastructure.sql

--- group5_db/ddl/tables/01_infrastructure.sql
+++ group5_db/ddl/tables/01_infrastructure.sql
@@ -7,7 +7,16 @@ primary key (campus_ID),
 check (campus_status in (‘AVAILABLE’, ’UNAVAILABLE’))
 );
 
-(building)
+create table building (
+campus_ID			bigint unsigned not null,
+building_ID			bigint unsigned auto_increment,
+building_name		varchar(255)
+building_status		varchar(11)
+
+primary key (building_ID),
+foreign key (campus_ID) references campus(campus_ID),
+check (campus_status in (‘AVAILABLE’, ’UNAVAILABLE’))
+);
 
 (floor)
 

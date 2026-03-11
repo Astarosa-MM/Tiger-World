af4eff1f0fa8eb8124804fa4b524c0ee1668f738
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-11 15:36:43 -0500

    campus -> 01_infrastructure.sql
    "create table" statement for campus table has been added to 01_infrastructure.sql

--- group5_db/ddl/tables/01_infrastructure.sql
+++ group5_db/ddl/tables/01_infrastructure.sql
@@ -1,4 +1,11 @@
-(campus)
+create table campus (
+campus_ID			bigint unsigned auto_increment,
+campus_name		varchar(255)
+campus_status		varchar(11)
+
+primary key (campus_ID),
+check (campus_status in (‘AVAILABLE’, ’UNAVAILABLE’))
+);
 
 (building)
 

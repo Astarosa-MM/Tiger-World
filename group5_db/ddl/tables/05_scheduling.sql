6df29b3e23a36871a02cd834d693d7d405e65496
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-11 16:05:48 -0500

    events -> 05_scheduling.sql
    "create table" statement for events table added to 05_scheduling.sql

--- group5_db/ddl/tables/05_scheduling.sql
+++ group5_db/ddl/tables/05_scheduling.sql
@@ -1,1 +1,13 @@
-(events)
+create table events (
+owner_ID		bigint unsigned,
+owner_type		char(4),
+
+event_ID		bigint unsigned auto_increment,
+event_name		varchar(255), 
+start_time		time,
+end_time		time,
+event_date		date,
+
+primary key (event_ID),
+check(owner_type in (‘USER’, ‘CAMPUS’, ‘BUILDING’, ‘FLOOR’, ‘ZONE’, ‘ROOM’))
+);

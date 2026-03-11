f82b55e51d670dace54b167296a01161df1a5cfd
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-11 16:05:05 -0500

    user_settings indentation edit

--- group5_db/ddl/tables/04_accounts.sql
+++ group5_db/ddl/tables/04_accounts.sql
@@ -11,7 +11,6 @@ check (email like '%.edu')
 create table user_settings (
 user_ID				bigint unsigned,
 default_campus			bigint unsigned,
-
 mode				varchar(5),
 time_zone			char(3),
 push_notifs			varchar(3),
@@ -19,7 +18,6 @@ push_notifs			varchar(3),
 primary key (user_ID),
 foreign key (user_ID) references user_info(user_ID),
 foreign key (default_campus) references campus(campus_ID),
-
 check(mode in (
 ‘LIGHT’, ‘DARK’)),
 check(time_zone in (

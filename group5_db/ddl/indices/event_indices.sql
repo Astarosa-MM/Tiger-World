17532fff25b149751a84276c1e5e66c0b56246fa
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-12 16:50:58 -0500

    details misspelled -> event_indices.sql

--- group5_db/ddl/indices/event_indices.sql
+++ group5_db/ddl/indices/event_indices.sql
@@ -1,6 +1,6 @@
 -- indices indo various compositions of attributes in infra_events table
 create index idx_ID_location on infra_events(event_ID, infra_type, infra_ID);
-create index idx_ID_detials on infra_events(event_ID, event_name, start_time, end_time);
+create index idx_ID_details on infra_events(event_ID, event_name, start_time, end_time);
 create index idx_ID_date on infra_events(event_ID, event_date);
 
 -- indices into user_ID and event_ID in user_events table respectively

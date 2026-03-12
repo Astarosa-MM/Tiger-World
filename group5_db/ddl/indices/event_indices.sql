b239231bb81ae9e0a8b7db24b1d240a44c2236e0
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-12 16:50:31 -0500

    done -> event_indices.sql

--- group5_db/ddl/indices/event_indices.sql
+++ group5_db/ddl/indices/event_indices.sql
@@ -1,21 +1,8 @@
--- INFRA_EVENTS table
+-- indices indo various compositions of attributes in infra_events table
+create index idx_ID_location on infra_events(event_ID, infra_type, infra_ID);
+create index idx_ID_detials on infra_events(event_ID, event_name, start_time, end_time);
+create index idx_ID_date on infra_events(event_ID, event_date);
 
-/* user looking at (infrastructure_type) schedule on a given day
- * display requirements: event name, start and end time
- * date verification requirements: date */
-create index idx_infrID_details on infra_events(infra_type, infra_ID, event_name, start_time, end_time);
-create index idx_infrID_date on infra_events(infra_type, infra_ID, event_date);
-
-
-/* user looking at their scheule on a given day
- * display requirements: location, event name, start and end time
- * date verification requirements: date */
-create index idx_eID_details on infra_events(event_ID, infra_type, infra_ID, event_name, start_time, end_time);
-create index idx_eID_date on infra_events(event_ID, event_date);
-
--- USER_EVENTS table
--- user_ID in many-to-many table (users and events)
+-- indices into user_ID and event_ID in user_events table respectively
 create index idx_ue_user on user_events(user_ID);
-
--- event_ID in many-to-many table (users and events)
 create index idx_ue_event on user_events(event_ID);

5afac4c061dadfddb5f6cfb688d356736230f7b3
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-12 16:33:10 -0500

    syntax fix -> event_indices.sql

--- group5_db/ddl/indices/event_indices.sql
+++ group5_db/ddl/indices/event_indices.sql
@@ -3,14 +3,14 @@
 /* user looking at (infrastructure_type) schedule on a given day
  * display requirements: event name, start and end time
  * date verification requirements: date */
-create index idx_IDK_details on infra_events(infra_type, infra_ID, event_name, start_time, end_time);
-create index idx_IDK_date on infra_events(infra_type, infra_ID, event_date);
+create index idx_infrID_details on infra_events(infra_type, infra_ID, event_name, start_time, end_time);
+create index idx_infrID_date on infra_events(infra_type, infra_ID, event_date);
 
 
 /* user looking at their scheule on a given day
  * display requirements: location, event name, start and end time
  * date verification requirements: date */
-create index idx eID_details on infra_events(event_ID, infra_type, infra_ID, event_name, start_time, end_time);
+create index idx_eID_details on infra_events(event_ID, infra_type, infra_ID, event_name, start_time, end_time);
 create index idx_eID_date on infra_events(event_ID, event_date);
 
 -- USER_EVENTS table

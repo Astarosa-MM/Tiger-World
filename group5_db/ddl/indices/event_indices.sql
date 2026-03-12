ecf880692be5cbd4ed5631917d85a07516aa2864
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-12 16:32:41 -0500

    work in progress -> event_indices.sql

--- group5_db/ddl/indices/event_indices.sql
+++ group5_db/ddl/indices/event_indices.sql
@@ -1,18 +1,19 @@
--- user looking at room schedule on a given day
--- display requirements: event name, start and end time
--- date verification requirements: date
-infra type, infra id, event name, start time, end time, date
+-- INFRA_EVENTS table
 
-infra type, infra id, date
+/* user looking at (infrastructure_type) schedule on a given day
+ * display requirements: event name, start and end time
+ * date verification requirements: date */
+create index idx_IDK_details on infra_events(infra_type, infra_ID, event_name, start_time, end_time);
+create index idx_IDK_date on infra_events(infra_type, infra_ID, event_date);
 
--- user looking at their scheule on a given day
--- display requirements: location, event name, start and end time
--- date verification requirements: date
-event id, infra type, infra id, event name, start time, end time, date
 
-event id, date
+/* user looking at their scheule on a given day
+ * display requirements: location, event name, start and end time
+ * date verification requirements: date */
+create index idx eID_details on infra_events(event_ID, infra_type, infra_ID, event_name, start_time, end_time);
+create index idx_eID_date on infra_events(event_ID, event_date);
 
-____________________________________________________________
+-- USER_EVENTS table
 -- user_ID in many-to-many table (users and events)
 create index idx_ue_user on user_events(user_ID);
 

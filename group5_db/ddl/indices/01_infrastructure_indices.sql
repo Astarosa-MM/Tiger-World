2ffb28323afeb50d3fd928f2127f6bb56254c4aa
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-12 17:07:24 -0500

    added indices for room, added ID to status indices -> 01_infrastructure_indices.sql

--- group5_db/ddl/indices/01_infrastructure_indices.sql
+++ group5_db/ddl/indices/01_infrastructure_indices.sql
@@ -1,11 +1,13 @@
 -- gives the database a target to jump to when (infrastructure_entity_type)_status is used in a query
 
-create index idx_campus_status on campus(campus_status);
+create index idx_campus_status on campus(campus_ID, campus_status);
 
-create index idx_building_status on building(building_status);
+create index idx_building_status on building(building_ID, building_status);
 
-create index idx_floor_status on floor(floor_status);
+create index idx_floor_status on floor(floor_ID, floor_status);
 
-create index idx_zone_status on zone(zone_status);
+create index idx_zone_status on zone(zone_ID, zone_status);
 
-create index idx_room_status on room(room_status);
+create index idx_room_status on room(room_ID, room_status);
+create index idx_room_number on room(room_ID, room_number);
+create index idx_room_classification on room(room_ID, room_classification);

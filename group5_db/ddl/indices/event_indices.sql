8b99484646b2f1ef4b7887868bf18d5b2b2875fe
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-12 14:45:51 -0500

    documentation tweak and rename owner_eventdates.sql to event_indices.sql

--- group5_db/ddl/indices/event_indices.sql
+++ group5_db/ddl/indices/event_indices.sql
@@ -6,3 +6,8 @@ create index idx_ue_user on user_events(user_ID);
 
 -- gives the database a target to jump to when event_ID is used in a query
 create index idx_ue_event on user_events(event_ID);
+
+/* primary keys are automatically indexed
+ * would have given the database a target to jump to when event_ID (from infra_events) is used in a query
+ * most notably, joins with user_events using 'event_ID' */
+-- create index idx_eventID_infra on infra_events(event_ID);

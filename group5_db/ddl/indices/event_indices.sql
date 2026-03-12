8b95b8447fb4a0851034893011008966136d22c8
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-12 16:23:27 -0500

    work in progress -> event_indices.sql

--- group5_db/ddl/indices/event_indices.sql
+++ group5_db/ddl/indices/event_indices.sql
@@ -1,13 +1,20 @@
--- gives the database a COMPOSITE target to jump to when infra_type, infra_ID, or event_date are used in a query
-create index idx_event_date_infra on infra_events(infra_type, infra_ID, event_date);
+-- user looking at room schedule on a given day
+-- display requirements: event name, start and end time
+-- date verification requirements: date
+infra type, infra id, event name, start time, end time, date
 
--- gives the database a target to jump to when user_ID is used in a query
+infra type, infra id, date
+
+-- user looking at their scheule on a given day
+-- display requirements: location, event name, start and end time
+-- date verification requirements: date
+event id, infra type, infra id, event name, start time, end time, date
+
+event id, date
+
+____________________________________________________________
+-- user_ID in many-to-many table (users and events)
 create index idx_ue_user on user_events(user_ID);
 
--- gives the database a target to jump to when event_ID is used in a query
+-- event_ID in many-to-many table (users and events)
 create index idx_ue_event on user_events(event_ID);
-
-/* primary keys are automatically indexed
- * would have given the database a target to jump to when event_ID (from infra_events) is used in a query
- * most notably, joins with user_events using 'event_ID' */
--- create index idx_eventID_infra on infra_events(event_ID);

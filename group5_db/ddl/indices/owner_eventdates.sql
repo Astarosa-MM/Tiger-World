e0ea0ba8140ad1b262347450b322211a8994ce6b
slevy7736 <111536484+slevy7736@users.noreply.github.com>
2026-03-12 12:40:24 -0500

    some documentation -> owner_eventdates.sql

--- group5_db/ddl/indices/owner_eventdates.sql
+++ group5_db/ddl/indices/owner_eventdates.sql
@@ -1,3 +1,9 @@
+/* indices support:
+ * - user_ID -> quickly retrieve all events for a specific user
+ * - infrastructure_type + infrastructure_ID -> quickly retrieve all events for a specific infrastructure entity
+
+ * - event_date -> allows filtering over single days or date ranges (BETWEEN) for both user and infrastructure queries */
+
 create index idx_event_date_user on events(user_ID, event_date);
 
 create index idx_event_date_infra on events(infrastructure_type, infrastructure_ID, event_date);

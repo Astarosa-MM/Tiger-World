afccdcf11650f8f679014da22875ab3a3f52a0cf
Brent Bolden <bbold16@lsu.edu>
2026-04-10 23:33:08 -0500

    Finished the basic sign up page
    Finished the sign up page. The user will be able to enter their email and password with the necessary checks in place to ensure that they are valid. The page does not save anything to the database yet.

--- pages/CopyPasswordField.tsx
+++ pages/CopyPasswordField.tsx
@@ -4,13 +4,28 @@ import { eye, eyeOff, lockClosed } from 'ionicons/icons';
 import { useState } from 'react';
 
 
-const CopyPasswordField: React.FC = () => {
+  interface Props {
+    onCopyFieldChange: (value: string) => void;
+  }
+
+const CopyPasswordField: React.FC<Props> = ({onCopyFieldChange}) => {
 
   const [showPassword, setShowPassword] = useState(false);
 
+  const handleCopyFieldChange = (e: CustomEvent) => {
+    const val = e.detail.value;
+    onCopyFieldChange(val);
+  };
+
   return (
     <div>
-        <IonInput type={showPassword ? 'text' : 'password'} color="tertiary" label="Password: " placeholder="Re-enter password" fill="solid">
+        <IonInput 
+          type={showPassword ? 'text' : 'password'}
+          color="tertiary" label="Password: " 
+          placeholder="Re-enter password" 
+          fill="solid"
+          onIonInput={handleCopyFieldChange}
+        >
           <IonIcon slot="start" icon={lockClosed} aria-hidden="true"></IonIcon>
           <IonButton fill="clear" slot="end" aria-label="Show/hide" onClick={() => setShowPassword(prev => !prev)}>
             <IonIcon color="tertiary" slot="icon-only" icon={showPassword ? eyeOff : eye} aria-hidden="true"></IonIcon>
--- pages/SignUp.tsx
+++ pages/SignUp.tsx
@@ -1,25 +1,66 @@
 import { IonButton, IonContent, IonHeader, IonIcon, IonInput, IonLabel, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './SignUp.css';
-import { arrowForward } from 'ionicons/icons';
+import { arrowForward, copy } from 'ionicons/icons';
 import { useState } from 'react';
 import PasswordField from './PasswordField';
 import CopyPasswordField from './CopyPasswordField';
 
 const SignUp: React.FC = () => {
   //remember to clear passwords when switching pages
-  const [passwordInvalidText, setPasswordInvalidText] = useState("");
-  const [password, setPassword] = useState("");
 
-  const handlePassordFieldChange = (val: string) =>{
-    setPassword(val);
+  const [emailInvalidText, setEmailInvalidText] = useState("");
+  const [emailValid, setEmailValid] = useState(false);
+
+  const [passwordInvalidText, setPasswordInvalidText] = useState("");
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [password, setPassword] = useState("");
+  const [copyPassword, setCopyPassword] = useState("");
+
+  const handlePassordFieldChange = (val : string) =>{
     //At Least 6 Characters with at least one alphabet, one digit, and one special character
-    const passwordTest =  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d]).{6,}$/;
-    if(passwordTest.test(val)){
+    const passwordRegex =  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d]).{6,}$/;
+    const isPasswordValid = passwordRegex.test(val);
+    setPassword(val);
+    if(isPasswordValid && val == copyPassword){
       setPasswordInvalidText("");
+      setPasswordValid(true);
+    }
+    else if (isPasswordValid && val != copyPassword){
+      setPasswordInvalidText("Passwords must match");
+      setPasswordValid(true);
     }
     else{
       setPasswordInvalidText("Password needs to be 6 characters with 1 letter, 1 digit, and one special character.");
+      setPasswordValid(false);
+    }
+  }
+
+
+  const handleCopyFieldChange = (val : string) => {
+    setCopyPassword(val);
+    if((val == password) && passwordValid){
+      setPasswordInvalidText("");
+    } 
+    else if (passwordValid && val != password){
+      setPasswordInvalidText("Passwords must match");
+    }
+    else {
+      setPasswordInvalidText("Password needs to be 6 characters with 1 letter, 1 digit, and one special character.");
+    }
+  }
+
+  const handleEmailFieldChange = (e : CustomEvent) => {
+    //An alphanumeric or digit ending in @lsu.edu Email
+    const passwordRegex =  /^[a-zA-Z][a-zA-Z0-9]*@lsu\.edu$/;
+
+    if(passwordRegex.test(e.detail.value)){
+      setEmailInvalidText("");
+      setEmailValid(true);
+    }
+    else{
+      setEmailInvalidText("You must enter a valid email.");
+      setEmailValid(false);
     }
   }
   return (
@@ -40,21 +81,26 @@ const SignUp: React.FC = () => {
           <h1>Sign Up</h1>
         </IonLabel>
 
-        <p>Enter a valid LSU email.</p>
+        <p>Enter a valid lsu.edu email.</p>
+        
+        <p className="email-invalid">{emailInvalidText}</p>
+        <div></div>
         <p className="password-invalid">{passwordInvalidText}</p>
         <IonInput 
           type="email" 
           color="tertiary" 
           label="University Email: " 
           placeholder="Enter your email" 
-          fill="solid">
+          fill="solid"
+          onIonInput={handleEmailFieldChange}
+        >
         </IonInput>
         
         <PasswordField onPasswordFieldChange={handlePassordFieldChange}/>
 
-        <CopyPasswordField/>
+        <CopyPasswordField onCopyFieldChange={handleCopyFieldChange}/>
       
-        <IonButton href="tab2" color="tertiary">
+        <IonButton href="tab2" color="tertiary" disabled= {(password == copyPassword && passwordValid && emailValid) ? false : true}>
           <IonLabel>Sign Up: </IonLabel>
           <IonIcon icon={arrowForward}></IonIcon>
         </IonButton>
